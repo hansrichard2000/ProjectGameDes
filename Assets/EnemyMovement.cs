3991ba40c1dc64ca31cc64effd832e0e2ea82d35
ReinerJasin <reinerjasin@gmail.com>
2020-12-26 23:49:46 +0800

    collider and rigid body and walk animation for enemy

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 0.03f;
+    float batasKanan = 9.1f;
+    float batasKiri = -9.1f;
+    float batasAtas = 5.2f;
+    float batasBawah = -5.2f;
+
+    Vector3 translationVec;
+    float x = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (transform.localPosition.x > batasKanan || transform.localPosition.x < batasKiri)
+        {
+            x = -x;
+        }
+
+        if (transform.localPosition.y > batasAtas || transform.localPosition.y < batasBawah)
+        {
+//            y = -y;
+        }
+
+        translationVec = new Vector3(x, 0, 0);
+
+        transform.localPosition += translationVec * speed;
+    }
+}
