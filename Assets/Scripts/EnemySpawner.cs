ba868ad68b7dcd7d2c0f6bf7cdbd16c7c640509a
ReinerJasin <reinerjasin@gmail.com>
2020-12-29 13:49:02 +0800

    make enemy spawn by pressing "A"

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public float positionX, positionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            positionX = Random.Range(0f, 8.5f);
+            positionY = Random.Range(-3f, 1f);
+
+            var newEnemy = Instantiate(enemy);
+            newEnemy.transform.position = new Vector3(positionX, positionY, 0);
+        }
+
+    }
+}
