7802db71f48e375b6afa4ff59edb4209af5fec6d
ReinerJasin <reinerjasin@gmail.com>
2020-12-31 15:16:41 +0800

    Directory cleaning and added tag for castle and enemy knight

--- Assets/Scripts/AllySpawner.cs
+++ Assets/Scripts/AllySpawner.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class AllySpawner : MonoBehaviour
 {
+
+    public GameObject[] ally;
+    public float positionX, positionY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,20 @@ public class AllySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnAlly();
+        }
+
+    }
+
+    void SpawnAlly()
+    {
+        positionX = Random.Range(-2.5f, -1.5f);
+        positionY = Random.Range(-2.75f, -3.75f);
+
+        var newAlly = Instantiate(ally[Random.Range(0, ally.Length)]);
+        newAlly.transform.position = new Vector3(positionX, positionY, 0);
     }
 }
